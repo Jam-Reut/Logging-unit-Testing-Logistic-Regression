29a62a97f7f60c0a539567b4e96e7c1a39f9859a
Jam-Reut <abderrahim.houbbadi@live.de>
2025-10-21 18:20:02 +0200

    Update logistic_model.py

--- logistic_model.py
+++ logistic_model.py
@@ -67,7 +67,6 @@ def train_model(df):
 
     model = LogisticRegression(max_iter=1000)
     model.fit(X_train, y_train)
-    print("  Modelltraining abgeschlossen.")
     return model, X_test, y_test
 
 
