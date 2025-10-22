f453dd10534ed22d01b0a3acd00ade9e2b0e0c07
Jam-Reut <abderrahim.houbbadi@live.de>
2025-10-22 16:00:05 +0200

    Update logistic_model.py

--- logistic_model.py
+++ logistic_model.py
@@ -22,7 +22,8 @@ def get_last_timing(func_name):
 @my_timer
 def load_data(file_path):
     print("\n=== Datensatz laden ===")
-    df = pd.read_csv(file_path)
+    print()
+	df = pd.read_csv(file_path)
     print(f"  Datei: {file_path} | Shape: {df.shape}")
     return df
 
