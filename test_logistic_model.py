7d4ace50bb59b37cc2077679eee06bedaede592d
Jam-Reut <abderrahim.houbbadi@live.de>
2025-10-24 13:23:07 +0200

    Update test_logistic_model.py

--- test_logistic_model.py
+++ test_logistic_model.py
@@ -27,9 +27,9 @@ class TestLogisticRegressionModel(unittest.TestCase):
     # TESTFALL 1: predict(): Vorhersagefunktion
     # ------------------------------------------------
     def test_1_predict_function(self):
-        #plain_logger.info("=" * 70)
-        #plain_logger.info("TESTFALL 1: predict(): Vorhersagefunktion")
-        #plain_logger.info("=" * 70 + "\n")
+        # plain_logger.info("=" * 70)
+        # plain_logger.info("TESTFALL 1: predict(): Vorhersagefunktion")
+        # plain_logger.info("=" * 70 + "\n")
 
         plain_logger.info("[TEST 1 LOGGING: Vorhersageprüfung]\n")
 
@@ -82,4 +82,4 @@ class TestLogisticRegressionModel(unittest.TestCase):
 if __name__ == "__main__":
     plain_logger.info("\n=== Starte Unit-Tests ===\n")
     plain_logger.info("\n=== TESTFALL 1: predict(): Vorhersagefunktion ===\n")
-	unittest.main(argv=[""], exit=False)
+    unittest.main(argv=[""], exit=False)
